028d1deb58d0bb3087a5e063604d0f23b90a7293
kleinre <reillyjklein@gmail.com>
2017-06-23 09:53:31 -0500

    Working changes background

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,7 @@ import { ParticleProvider } from '../../providers/particle/particle';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public var1: any;
+  public rgb: any;
   private subscription: any = null;
   
   constructor(public navCtrl: NavController, public particle: ParticleProvider) {
@@ -28,10 +28,10 @@ export class HomePage {
   ionViewDidEnter() {
     if (this.particle.device) {
         this.cancelSubscription();
-        this.particle.pollVariable("var1").subscribe(
-            (value) => { this.var1 = value; },
-            (error) => { console.log("Error reading var1"); },
-            () => { console.log("Stopped polling var1"); }
+        this.particle.pollVariable("rgb").subscribe(
+            (value) => { this.rgb = value; },
+            (error) => { console.log("Error reading rgb"); },
+            () => { console.log("Stopped polling rgb"); }
         );
     }
   }
